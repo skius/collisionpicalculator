1726128d4b35dd09cb587a661e59f3e6418f2ad8
Niels Saurer <me@nielssaurer.com>
2019-03-18 16:31:54 +0100

    go fmt -> spaces to tabs again

--- block.go
+++ block.go
@@ -7,8 +7,8 @@ type Block struct {
 
 func Bounce(b1, b2 *Block) {
 	sumM := b1.m + b2.m
-	newV1 := ((b1.m - b2.m)/sumM) * b1.v + (2*b2.m/sumM) * b2.v
-	newV2 := (2*b1.m/sumM) * b1.v + (b2.m - b1.m)/sumM * b2.v
+	newV1 := ((b1.m-b2.m)/sumM)*b1.v + (2*b2.m/sumM)*b2.v
+	newV2 := (2*b1.m/sumM)*b1.v + (b2.m-b1.m)/sumM*b2.v
 	b1.v = newV1
 	b2.v = newV2
 }
--- main.go
+++ main.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-    "math"
-    "fmt"
-    "time"
-    "strconv"
-    "os"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-    var digits int
-    if len(os.Args) > 1 {
-        digits, _ = strconv.Atoi(os.Args[1])
-    } else {
-        digits = 10
-    }
+	var digits int
+	if len(os.Args) > 1 {
+		digits, _ = strconv.Atoi(os.Args[1])
+	} else {
+		digits = 10
+	}
 
-    b1 := Block{
-        m: 1,
-        v: 0,
-    }
-    b2 := Block{
-        m: math.Pow(100, float64(digits-1)),
-        v: -1,
-    }
+	b1 := Block{
+		m: 1,
+		v: 0,
+	}
+	b2 := Block{
+		m: math.Pow(100, float64(digits-1)),
+		v: -1,
+	}
 
-    var count int64 = 0
-    ticker := time.NewTicker(time.Millisecond*200)
-    go func () {
-        for range ticker.C {
-            fmt.Println(count)
-        }
+	var count int64 = 0
+	ticker := time.NewTicker(time.Millisecond * 200)
+	go func() {
+		for range ticker.C {
+			fmt.Println(count)
+		}
 	}()
-    before := time.Now()
-    bounce := true
-    for {
-        if b1.v >= 0 && b2.v > b1.v {
-            break
-        }
-        if bounce {
-            Bounce(&b1, &b2)
-            //fmt.Printf("Ratio: %.10f\n", b2.v/b1.v)
-        } else {
-            b1.Reverse()
-        }
-        bounce = !bounce
-        count++
-    }
+	before := time.Now()
+	bounce := true
+	for {
+		if b1.v >= 0 && b2.v > b1.v {
+			break
+		}
+		if bounce {
+			Bounce(&b1, &b2)
+			//fmt.Printf("Ratio: %.10f\n", b2.v/b1.v)
+		} else {
+			b1.Reverse()
+		}
+		bounce = !bounce
+		count++
+	}
 
-    after := time.Now()
-    passed := after.Sub(before).Nanoseconds()/1000/1000
+	after := time.Now()
+	passed := after.Sub(before).Nanoseconds() / 1000 / 1000
 
-    fmt.Println("Calculated", digits, "digits of Pi:", float64(count)/math.Pow(10, float64(digits-1)), "in", passed, "ms.")
+	fmt.Println("Calculated", digits, "digits of Pi:", float64(count)/math.Pow(10, float64(digits-1)), "in", passed, "ms.")
 }
